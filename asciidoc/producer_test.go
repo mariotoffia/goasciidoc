f93f015083d55b92660cf8ea2ed056cbb5bdb054
Mario Toffia <mario.toffia@gmail.com>
2020-11-03 18:45:38 +0100

    fixed tests

--- asciidoc/producer_test.go
+++ asciidoc/producer_test.go
@@ -5,12 +5,17 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/mariotoffia/goasciidoc/internal/utils"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerateWorkspaceToString(t *testing.T) {
+
+	// Make sure to cleanup before copy dir
+	time.Sleep(1 * time.Second)
+
 	target, err := utils.TempCopyDir("../", "asciidoc-tests")
 	if err != nil {
 		assert.FailNow(t, err.Error())
